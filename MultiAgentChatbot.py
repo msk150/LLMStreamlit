f4cd0cd17aa21c66a82a89a7c0ea8b4049160c04
msk150 <64040707+msk150@users.noreply.github.com>
2024-03-14 15:43:24 -0700

    Update MultiAgentChatbot.py

--- MultiAgentChatbot.py
+++ MultiAgentChatbot.py
@@ -5,8 +5,8 @@ from openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import Pinecone
 
-OPENAI_API_KEY = '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'
-openai.api_key = OPENAI_API_KEY
+# OPENAI_API_KEY = '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'
+# openai.api_key = OPENAI_API_KEY
 
 def get_completion(client, prompt, messages, model="gpt-3.5-turbo"):
     # append the new message after the previous messages
@@ -107,7 +107,7 @@ class Relevant_Documents_Agent:
 class Head_Agent:
     def __init__(self, pinecone_index_name='mini-project-2', topic="machine learning") -> None:
         # TODO: Initialize the Head_Agent
-        self.client = OpenAI()
+        self.client = OpenAI(api_key='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')
         self.embeddings = OpenAIEmbeddings()
         self.pinecone_index_name = pinecone_index_name
         self.topic = topic
